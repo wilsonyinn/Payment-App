49818188576a29e4236fef8b7bc7b4f5212c9258
wilsonyinn <68367621+wilsonyinn@users.noreply.github.com>
2022-06-04 17:08:50 -0700

    cards are now properly displayed on page

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -3,13 +3,15 @@ import Home from "../components/Home";
 import Slider from "../components/Slider";
 import Navigation from "../components/Navigation";
 import TeamCard from "../components/TeamCard";
+import styles from "../styles.module.css";
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
       <Slider />
-      <div className="teamSection">
+      <h2 className={styles.teamTitle}>Meet the Developers</h2>
+      <div className={styles.teamSection}>
         <TeamCard
           name="Tolby Lam"
           description="Computer Science Student"
