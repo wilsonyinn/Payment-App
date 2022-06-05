342b4e239723a60d00fe6f52d59cf3e29bc02320
wilsonyinn <68367621+wilsonyinn@users.noreply.github.com>
2022-06-04 17:00:56 -0700

    properly sized photos

--- front-end/src/components/TeamCard.js
+++ front-end/src/components/TeamCard.js
@@ -6,7 +6,7 @@ const TeamCard = ({ name, description, img }) => {
   return (
     <Card style={{ width: "18rem" }}>
       {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-      <Card.Img variant="top" src={img} />
+      <Card.Img variant="top" src={img} style = {{ width: "18rem", height: 300}}/>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import Home from '../components/Home'
-import Slider from '../components/Slider'
-import Navigation from '../components/Navigation'
-import TeamCard from '../components/TeamCard'
+import React from "react";
+import Home from "../components/Home";
+import Slider from "../components/Slider";
+import Navigation from "../components/Navigation";
+import TeamCard from "../components/TeamCard";
 
 const HomePage = () => {
   return (
     <div>
       <Navigation />
       <Slider />
-      <TeamCard />
+      <div className="teamSection">
+        <TeamCard
+          name="Tolby Lam"
+          description="Computer Science Student"
+          img="../assets/tolby-lam.jpeg"
+        />
+        <TeamCard
+          name="Wilson Yin"
+          description="Software Engineer"
+          img="../assets/wilson-yin.jpg"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
